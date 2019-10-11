526e0837ed4d27e7a38bdb39bb4bca02d71d73e2
Mikhail Eremkin <eremkin@devexperts.com>
2019-10-11 16:59:02 +0300

    Refactoring TranslationUpdater

--- src/main/kotlin/me/eremkin/lokalise/tasks/TranslationUpdater.kt
+++ src/main/kotlin/me/eremkin/lokalise/tasks/TranslationUpdater.kt
@@ -4,6 +4,7 @@ import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import java.io.File
+import java.io.StringWriter
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
@@ -14,7 +15,11 @@ object TranslationUpdater {
 
     fun mergeStrings(orgXml: File, newXml: File, resultXml: File = orgXml) {
 
-        val finalDocument = mergeXml(orgXml, newXml)
+        val document = mergeXml(orgXml, newXml)
+        val domSource = DOMSource(document)
+
+        val textStream = StreamResult(StringWriter())
+        val resultStream = StreamResult(resultXml)
 
         TransformerFactory.newInstance().newTransformer()
             .apply {
@@ -23,14 +28,12 @@ object TranslationUpdater {
                 setOutputProperty(OutputKeys.INDENT, "yes")
                 setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
-                val domSource = DOMSource(finalDocument)
-                val streamResult = StreamResult(resultXml)
-                transform(domSource, streamResult)
+                transform(domSource, resultStream)
             }
     }
 
-    private fun mergeXml(originalContent: File, newContent: File): Document {
-        return DocumentBuilderFactory.newInstance().newDocumentBuilder().run {
+    private fun mergeXml(originalContent: File, newContent: File): Document =
+        DocumentBuilderFactory.newInstance().newDocumentBuilder().run {
             val xmlResultDoc = newDocument()
             val xmlResultEle = xmlResultDoc.createElement("resources")
             xmlResultDoc.appendChild(xmlResultEle)
@@ -38,52 +41,49 @@ object TranslationUpdater {
             val xmlOrgDoc = parse(originalContent)
             val xmlOrgEle = xmlOrgDoc.getDocumentElement()
 
-            println("Root Node:" + xmlOrgEle.nodeName)
             val stringsOrg = xmlOrgDoc.getElementsByTagName("string")
-            println("Original strings:" + stringsOrg.length)
+            println("Original file contains ${stringsOrg.length} strings")
 
             val xmlNewgDoc = parse(newContent)
             val stringsNew = xmlNewgDoc.getElementsByTagName("string")
-            println("New strings:" + stringsNew.length)
+            println("New file contains ${stringsNew.length} strings")
 
-            val newStringMap = mutableMapOf<String, Node>().let { map ->
-                with(stringsNew) {
-                    for (i in 0 until length) {
-                        val string = item(i) as Element
-                        val id = string.attributes.getNamedItem("name").nodeValue
-                        map[id] = string
-                    }
+            val newStringMap = mutableMapOf<String, Node>()
+
+            with(stringsNew) {
+                for (i in 0 until length) {
+                    val element = item(i) as Element
+                    val id = element.attributes.getNamedItem("name").nodeValue
+                    newStringMap[id] = element
                 }
-                map
             }
 
             println("Updating existing entries")
             with(stringsOrg) {
                 for (i in 0 until length) {
-                    val string = item(i) as Element
-                    val id = string.attributes.getNamedItem("name").nodeValue
+                    val element = item(i) as Element
+                    val id = element.attributes.getNamedItem("name").nodeValue
                     newStringMap[id]?.let {
-                        string.textContent = it.textContent
+                        element.textContent = it.textContent
                         newStringMap.remove(id)
                     }
                 }
             }
 
-            with(xmlOrgEle.childNodes){
-                for(i in 0 until length){
-                    val xmlImportedNode = xmlResultDoc.importNode(item(i), true)
-                    xmlResultEle.appendChild(xmlImportedNode)
+            with(xmlOrgEle.childNodes) {
+                for (i in 0 until length) {
+                    val node = xmlResultDoc.importNode(item(i), true)
+                    xmlResultEle.appendChild(node)
                 }
             }
 
-            println("Writing ${newStringMap.size} new entries")
             if (newStringMap.isNotEmpty()) {
-                for (node in newStringMap.values) {
-                    val xmlImportedNode = xmlResultDoc.importNode(node, true)
-                    xmlResultEle.appendChild(xmlImportedNode)
+                println("Writing ${newStringMap.size} new entries")
+                for (stringNode in newStringMap.values) {
+                    val node = xmlResultDoc.importNode(stringNode, true)
+                    xmlResultEle.appendChild(node)
                 }
             }
             xmlResultDoc
         }
-    }
 }
